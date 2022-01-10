694acdaf0106c0a537e4f197aad0cd2ed3a8abcb
Kirill Machnev <machnev999@gmail.com>
2022-01-10 20:17:56 +0300

    add file

---
+++ Miles.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        int amount = 3700;
+        int rubles = 20;
+
+        int bonus = amount / rubles;
+        System.out.println(bonus);
+    }
+}
